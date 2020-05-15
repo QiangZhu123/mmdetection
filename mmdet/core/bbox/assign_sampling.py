d12bd1c79d8aed52bb76eb233ea1d308ce4ad79c
ZAQP <34334567+QiangZhu123@users.noreply.github.com>
2020-05-15 16:45:50 +0800

    Update assign_sampling.py

--- mmdet/core/bbox/assign_sampling.py
+++ mmdet/core/bbox/assign_sampling.py
@@ -2,7 +2,7 @@ import mmcv
 
 from . import assigners, samplers
 
-
+#对于分配和采样，都有一个专门的函数进行字典参数调用
 def build_assigner(cfg, **kwargs):
     if isinstance(cfg, assigners.BaseAssigner):
         return cfg
@@ -22,7 +22,7 @@ def build_sampler(cfg, **kwargs):
         raise TypeError('Invalid type {} for building a sampler'.format(
             type(cfg)))
 
-
+#用一个函数实现分配和采样
 def assign_and_sample(bboxes, gt_bboxes, gt_bboxes_ignore, gt_labels, cfg):
     bbox_assigner = build_assigner(cfg.assigner)
     bbox_sampler = build_sampler(cfg.sampler)
